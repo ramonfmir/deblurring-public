023e20e270a191a66af17b0e4c25d18a833aa942
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2017-10-20 16:03:14 +0100

    [CH] CHanged some stuff.

--- deblurrer/hawkes_algorithm/character_splitter.py
+++ deblurrer/hawkes_algorithm/character_splitter.py
@@ -5,32 +5,78 @@ import cv2
 
 from blurrer import perspective_transformation as pt
 
+# Percentage tolerance of intersection
+TOLERANCE = 0.15
+
+def contain(rectA, rectB):
+    x1,y1,w1,h1 = rectA
+    x2,y2,w2,h2 = rectB
+
+    return (x1 < x2) and (y1 < y2) and (x1 + w1 > x2 + w2) and (y1 + h1 > y2 + h2)
+
 # Helper methods
+def intersection(rectA, rectB):
+    # Pre -- Assume non-zero rectangles
+    x1,y1,w1,h1 = rectA
+    x2,y2,w2,h2 = rectB
+
+    intersectWidth  = 0
+    intersectHeight = 0
+    # Check if widths intersect
+    if x1 <= x2:
+        if (x2 - x1) >= w1:
+            return False
+        intersectWidth = x1 + w1 - x2
+    else:
+        if (x1 - x2) >= w2:
+            return False
+        intersectWidth = x2 + w2 - x1
+
+    # Check if heights intersect
+    if y1 <= y2:
+        if (y2 - y1) >= h1:
+            return False
+        intersectHeight = y1 + h1 - y2
+    else:
+        if (y1 - y2) >= h2:
+            return False
+        intersectHeight = y2 + h2 - y1
+
+    if contain(rectA, rectB) or contain(rectB, rectA):
+        return True
+
+    intersect_area = float(intersectWidth * intersectHeight)
+    area_of_both = rect_area(rectA) + rect_area(rectB) - intersect_area
+
+    # Check tolerance
+    if (intersect_area / area_of_both) < TOLERANCE:
+        return False
+
+    return True
+
+def rect_area(rect):
+    x,y,w,h = rect
+    return w * h
+
 def group_rectangles(rectangles):
-    return _group_rectangles(rectangles, [])
+    grouped = []
+    for rectA in rectangles:
+        intersected = False
+        for rectB in grouped:
+            if intersection(rectA, rectB):
+                print(rectA,rectB)
+                intersected = True
+                areaA = rect_area(rectA)
+                areaB = rect_area(rectB)
+                if areaA > areaB:
+                    grouped.remove(rectB)
+                    grouped.append(rectA)
+                    break
 
-def _group_rectangles(input_rectangles, output_rectangles):
-    if not input_rectangles:
-        return output_rectangles
-
-    curr_rect = input_rectangles.pop()
-    has_intersected = False
-    for i in range(len(output_rectangles)):
-        other_rect = output_rectangles[i]
-        if intersection(curr_rect, other_rect):
-            has_intersected = True
-            area_curr = cv2.contourArea(curr_rect)
-            area_other = cv2.contourArea(curr_rect)
-            if area_curr > area_other:
-                del other_rect
-                output_rectangles.append(curr_rect)
-                has_intersected = False
-                break
-
-    if not has_intersected:
-        output_rectangles.append(curr_rect)
-
-    return _group_rectangles(input_rectangles, output_rectangles)
+        if not intersected:
+            grouped.append(rectA)
+    # Return list of non-intersecting rectangles
+    return grouped
 
 if __name__ == "__main__":
     img = cv2.imread(sys.argv[1])
@@ -53,6 +99,7 @@ if __name__ == "__main__":
             all_rectangles.append(cv2.boundingRect(cnt))
 
     rectangles = group_rectangles(all_rectangles)
+
     for rect in rectangles:
         [x, y, w, h] = rect
         if  h > 16 and h > w:
