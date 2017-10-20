33921713cbf090caa6c7ed173f142ae3de689423
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2017-10-20 13:36:22 +0100

    [AP][CH][RF] Testing the character splitter with the perspective transformations.

--- deblurrer/hawkes_algorithm/character_splitter.py
+++ deblurrer/hawkes_algorithm/character_splitter.py
@@ -3,66 +3,37 @@ import sys
 import numpy as np
 import cv2
 
-im = cv2.imread('../../data/4000unlabeledLP/00a5b119-a673-4800-b61f-dd4e64cd5364-0.jpg')
-#im = cv2.imread('numbers.png')
-im3 = im.copy()
-"""
-gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray,(7,7),0)
-blur = cv2.medianBlur(blur, 5)
-size = 10
-kernel_motion_blur = np.zeros((size, size))
-kernel_motion_blur[:, int((size - 1) / 2)] = np.ones(size)
-kernel_motion_blur = kernel_motion_blur / size
-blur = cv2.filter2D(blur, -1, kernel_motion_blur)
-"""
-blur = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+from blurrer import perspective_transformation as pt
 
-cv2.imshow('norm', blur)
-cv2.waitKey(0)
+img = cv2.imread(sys.argv[1])
 
-w,h = blur.shape
-print str(w) + " " + str(h)
-pts1 = np.float32([[0,h],[w,h], [0,0],[w,0]])
-pts2 = np.float32([[6,24],[66,31], [8,1],[68,12]])
-M = cv2.getPerspectiveTransform(pts1,pts2)
-blur = cv2.warpPerspective(blur,M,(6,68))
+# Image pre-processing.
 
-#thresh = cv2.adaptiveThreshold(blur,255,1,1,11,2)
+img = pt.apply_perspective(img, 10, 0)
 
-cv2.imshow('norm', blur)
-cv2.waitKey(0)
+# Finding characters.
 
-#################      Now finding Contours         ###################
-"""
-_,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
 
-samples =  np.empty((0,100))
-responses = []
-keys = [i for i in range(48,58)]
+_, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
+rectangles = []
 for cnt in contours:
-    if cv2.contourArea(cnt)>5:
-        [x,y,w,h] = cv2.boundingRect(cnt)
+    area = cv2.contourArea(cnt)
+    if area > 30 and area < 700:
+        rectangles.append(cv2.boundingRect(cnt))
 
-        if  h > 16 and h > w:
-            cv2.rectangle(im,(x,y),(x+w,y+h),(0,0,255),2)
-            roi = thresh[y:y+h,x:x+w]
-            roismall = cv2.resize(roi,(10,10))
-            cv2.imshow('norm',im)
-            key = cv2.waitKey(0)
+rectangles, _ = cv2.groupRectangles(rectangles, 0, 0.5)
 
-            if key == 27:  # (escape to quit)
-                sys.exit()
-            elif key in keys:
-                responses.append(int(chr(key)))
-                sample = roismall.reshape((1,100))
-                samples = np.append(samples,sample,0)
+for rect in rectangles:
+    [x, y, w, h] = rect
+    if  h > 16 and h > w:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        roi = thresh[y:(y + h), x:(x + w)]
+        roismall = cv2.resize(roi, (10, 10))
 
-responses = np.array(responses,np.float32)
-responses = responses.reshape((responses.size,1))
-print "training complete"
+cv2.imshow('Characters', img)
+cv2.waitKey(0)
 
-np.savetxt('generalsamples.data',samples)
-np.savetxt('generalresponses.data',responses)
-"""
+# TODO: Splitting into regions.
