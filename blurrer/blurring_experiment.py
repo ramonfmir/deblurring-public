f6d49f435ded837d46055078bbd44f95fa4eb7cf
Aristomenis-Dionysios Papadopoulos <arhpap@hotmail.com>
2017-10-21 20:14:41 +0100

    [AP] Playing around with the blurrer, I was told we were doing deblurring but whatever.

--- blurrer/blurring_experiment.py
+++ blurrer/blurring_experiment.py
@@ -1,38 +1,44 @@
-# Inspired by:
+# inspired by:
 # * http://www.geeksforgeeks.org/opencv-python-program-to-blur-an-image/
 # * https://www.packtpub.com/mapt/book/application_development/9781785283932/2/ch02lvl1sec21/motion-blur
+import sys
 
 import cv2
 import numpy as np
-img = cv2.imread('license_plate.jpg')
 
-# Averaging
-avging = cv2.blur(img, (10, 10))
-cv2.imshow('Averaging',avging)
-cv2.waitKey(0)
+def apply_blur(img, aver, gaussian, median, motion):
+    # Averaging
+    img = cv2.blur(img, (10, 10))
 
-# Gaussian Blurring
-gausBlur = cv2.GaussianBlur(img, (5, 5),0)
-cv2.imshow('Gaussian Blurring', gausBlur)
-cv2.waitKey(0)
+    # Gaussian Blurring
+    img = cv2.GaussianBlur(img, (5, 5),0)
 
-# Median blurring
-medBlur = cv2.medianBlur(img, 5)
-cv2.imshow('Media Blurring', medBlur)
-cv2.waitKey(0)
+    # Median blurring
+    img = cv2.medianBlur(img, 5)
 
-# Bilateral Filtering
-bilFilter = cv2.bilateralFilter(img, 9, 75, 75)
-cv2.imshow('Bilateral Filtering', bilFilter)
-cv2.waitKey(0)
+    # Bilateral Filtering
+    img = cv2.bilateralFilter(img, 9, 75, 75)
 
-# Motion blurring
-size = 10
-kernel_motion_blur = np.zeros((size, size))
-kernel_motion_blur[:, int((size - 1) / 2)] = np.ones(size)
-kernel_motion_blur = kernel_motion_blur / size
-output = cv2.filter2D(img, -1, kernel_motion_blur)
-cv2.imshow('Motion Blur', output)
-cv2.waitKey(0)
+    # Motion blurring
+    size = 10
+    kernel_motion_blur = np.zeros((size, size))
+    kernel_motion_blur[:, int((size - 1) / 2)] = np.ones(size)
+    kernel_motion_blur = kernel_motion_blur / size
+    img = cv2.filter2D(img, -1, kernel_motion_blur)
 
-cv2.destroyAllWindows()
+    return img
+
+if __name__ == "__main__":
+    img_name = sys.argv[1]
+    aver     = int(sys.argv[2])
+    gaussian = int(sys.argv[3])
+    median   = int(sys.argv[4])
+    motion   = int(sys.argv[5])
+
+    img = cv2.imread(img_name)
+
+    img = apply_blur(img, aver, gaussian, median, motion)
+
+    cv2.imshow('Blur', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
