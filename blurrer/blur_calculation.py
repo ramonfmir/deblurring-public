5c468235655b5d400d19f800b7a139a81d58a4d7
Aristomenis-Dionysios Papadopoulos <arhpap@hotmail.com>
2017-11-01 22:12:09 +0000

    [RF][AP] Bad metrics. Bad results. Bad commit messages. Good times.

--- blurrer/blur_calculation.py
+++ blurrer/blur_calculation.py
@@ -1,2 +1,15 @@
-# Sharpness of an image calculator using a Laplace filter and picking a max
-# value
+# Different metrics to detect blur in images. Adapted from: 
+# https://www.pyimagesearch.com/2015/09/07/blur-detection-with-opencv/
+
+import sys
+import numpy as np
+import cv2
+
+
+def simple_detection(img):
+   return cv2.Laplacian(img, cv2.CV_64F).var()
+
+if __name__ == "__main__":
+    img_name   = sys.argv[1]
+    img = cv2.imread(img_name)
+    print(float(simple_detection(img)))
