b9541568c25eeda4319aef00ff9d1d4d6f622a36
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2017-11-05 12:48:11 +0000

    [RF] Secure deployment.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  az login -u $AZURE_LOGIN_USR -p $AZURE_LOGIN_PWD
+  az vm start -g $AZURE_VM_GROUP -n $AZURE_VM_NAME
+  sleep 60
+  sshpass -p $AZURE_SSH_PWD ssh $AZURE_SSH_USR@$AZURE_HOST -o stricthostkeychecking=no 'bash deblurring/run.sh'
+  az vm stop -g $AZURE_VM_GROUP -n $AZURE_VM_NAME
+fi
--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ function run() {
   pip3 install --upgrade pip
   pip3 install -r requirements_azure.txt
   python3 data/dataset_manipulation/generate_dataset_same_dimensions.py
-  python3 deblurrer/cnn_autoencoder/train.py --run restart --num_iter 1 --model_name tutorial_cnn
+  python3 cnn_autoencoder/train.py --run restart --num_iter 1 --model_name tutorial_cnn
   ./slack_file_upload output.txt "#performance-metrics"
   rm -rf data/4000unlabeledLP_same_dims_scaled
   rm output.txt
