d32dec5980065a67dddc4d5f118db3f6aaadb2c9
Robert <Robert@dyn1177-187.wlan.ic.ac.uk>
2017-11-07 21:22:27 +0000

    [CH][RH] Added final convolution layer.

--- cnn_denoiser/model_definitions/networks/tutorial_cnn.py
+++ cnn_denoiser/model_definitions/networks/tutorial_cnn.py
@@ -12,18 +12,21 @@ def autoencoder(inputs, batch_size):
     # net = lays.conv2d(net, 64, [5, 5], stride=2, padding='SAME')
     # tf.summary.image('conv2', tf.expand_dims(tf.transpose(net, [3, 0, 1, 2])[0], 3), max_outputs=1)
     # print(net.shape)
-    net = lays.conv2d(net, 64, [5, 5], stride=1, padding='SAME')
+    net = lays.conv2d(net, 32, [30, 15], stride=10, padding='SAME')
     print(net.shape)
     tf.summary.image('code_layer', tf.expand_dims(tf.transpose(net, [3, 0, 1, 2])[0], 3), max_outputs=1)
 
     # Decoder
-    net = lays.conv2d_transpose(net, 64, [5, 5], stride=1, padding='SAME')
+    net = lays.conv2d_transpose(net, 64, [30, 15], stride=10, padding='SAME')
     tf.summary.image('deconv1', tf.expand_dims(tf.transpose(net, [3, 0, 1, 2])[0], 3), max_outputs=1)
     print(net.shape)
     # net = lays.conv2d_transpose(net, 256, [5, 5], stride=2, padding='SAME')
     # tf.summary.image('deconv2', tf.expand_dims(tf.transpose(net, [3, 0, 1, 2])[0], 3), max_outputs=1)
     # print(net.shape)
-    net = lays.conv2d_transpose(net, channels, [6, 6], stride=3, padding='SAME', activation_fn=tf.nn.tanh)
+    net = lays.conv2d_transpose(net, channels, [5, 5], stride=3, padding='SAME', activation_fn=tf.nn.tanh)
+    print(net.shape)
+
+    net = lays.conv2d(net, channels, [5, 5], stride=1, padding='SAME')
     print(net.shape)
 
     return net
