a75a588872c5fa18599f129bbdfbdca060df6d28
Robert <Robert@dyn1177-77.wlan.ic.ac.uk>
2017-11-07 11:00:10 +0000

    [RH] Added more filters to tutorial_cnn.

--- cnn_denoiser/model_definitions/networks/tutorial_cnn.py
+++ cnn_denoiser/model_definitions/networks/tutorial_cnn.py
@@ -6,17 +6,17 @@ channels = 1
 
 def autoencoder(inputs, batch_size):
     # Encoder
-    net = lays.conv2d(inputs, 128, [5, 5], stride=3, padding='SAME')
+    net = lays.conv2d(inputs, 256, [5, 5], stride=3, padding='SAME')
     print(net.shape)
-    net = lays.conv2d(net, 64, [5, 5], stride=2, padding='SAME')
+    net = lays.conv2d(net, 128, [5, 5], stride=2, padding='SAME')
     print(net.shape)
-    net = lays.conv2d(net, 32, [5, 5], stride=1, padding='SAME')
+    net = lays.conv2d(net, 64, [5, 5], stride=1, padding='SAME')
     print(net.shape)
 
     # Decoder
-    net = lays.conv2d_transpose(net, 64, [5, 5], stride=1, padding='SAME')
+    net = lays.conv2d_transpose(net, 128, [5, 5], stride=1, padding='SAME')
     print(net.shape)
-    net = lays.conv2d_transpose(net, 128, [5, 5], stride=2, padding='SAME')
+    net = lays.conv2d_transpose(net, 256, [5, 5], stride=2, padding='SAME')
     print(net.shape)
     net = lays.conv2d_transpose(net, channels, [5, 5], stride=3, padding='SAME', activation_fn=tf.nn.tanh)
     print(net.shape)
