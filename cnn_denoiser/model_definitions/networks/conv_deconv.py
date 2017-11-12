6293c95a7a17952a985b0011dbfc961319186747
Robert <Robert@dyn1178-25.wlan.ic.ac.uk>
2017-11-12 17:03:14 +0000

    [RH] Refactored conv_deconv and added ReLU to each layer (not sure where it went).

--- cnn_denoiser/model_definitions/networks/conv_deconv.py
+++ cnn_denoiser/model_definitions/networks/conv_deconv.py
@@ -7,31 +7,26 @@ def summary_layer(net, name):
     tf.summary.image(name, tf.expand_dims(tf.transpose(net, [3, 0, 1, 2])[0], 3), max_outputs=1)
     print(net.shape)
 
+def conv_layer_dropout(net, layer, out_channels, filter_dims, strides, padding, name, dropout, act_f = tf.nn.relu):
+    net = tf.layers.dropout(net, dropout)
+    return conv_layer(net, layer, out_channels, filter_dims, strides, padding, name, act_f)
+
+def conv_layer(net, layer, out_channels, filter_dims, strides, padding, name, act_f = tf.nn.relu):
+    net = layer(net, out_channels, filter_dims, strides=strides, padding=padding)
+    net = act_f(net)
+    summary_layer(net, name)
+    return net
+
 def autoencoder(inputs, batch_size, dropout=0.65):
     # Encoder
-    net = tf.layers.conv2d(inputs, 256, [5, 5], strides=(3, 3), padding='SAME')
-    summary_layer(net, 'conv1')
-
-    net = tf.layers.dropout(net, dropout)
-    net = tf.layers.conv2d(net, 128, [5, 5], strides=(2, 2), padding='SAME')
-    summary_layer(net, 'conv2')
-
-    net = tf.layers.dropout(net, dropout)
-    net = tf.layers.conv2d(net, 64, [5, 5], strides=(1, 1), padding='SAME')
-    summary_layer(net, 'conv3')
+    net =      conv_layer(inputs, tf.layers.conv2d, 256, [5, 5], (3, 3), 'SAME', 'conv1')
+    net = conv_layer_dropout(net, tf.layers.conv2d, 128, [5, 5], (2, 2), 'SAME', 'conv2', dropout)
+    net = conv_layer_dropout(net, tf.layers.conv2d, 64 , [5, 5], (1, 1), 'SAME', 'conv3', dropout)
 
     # Decoder
-    net = tf.layers.dropout(net, dropout)
-    net = tf.layers.conv2d_transpose(net, 64, [5, 5], strides=(1, 1), padding='SAME')
-    summary_layer(net, 'deconv1')
-
-    net = tf.layers.dropout(net, dropout)
-    net = tf.layers.conv2d_transpose(net, 128, [5, 5], strides=(2, 2), padding='SAME')
-    summary_layer(net, 'deconv2')
-
-    net = tf.layers.dropout(net, dropout)
-    net = tf.layers.conv2d_transpose(net, channels, [5, 5], strides=(3, 3), padding='SAME')
-    summary_layer(net, 'deconv3')
+    net = conv_layer_dropout(net, tf.layers.conv2d_transpose, 64 , [5, 5], (1, 1), 'SAME', 'deconv1', dropout)
+    net = conv_layer_dropout(net, tf.layers.conv2d_transpose, 128, [5, 5], (2, 2), 'SAME', 'deconv2', dropout)
+    net = conv_layer_dropout(net, tf.layers.conv2d_transpose, channels, [5, 5], (3, 3), 'SAME', 'deconv3', dropout)
 
     # Final tanh activation
     net = tf.nn.tanh(net)
