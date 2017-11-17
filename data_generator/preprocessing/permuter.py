a04ec353b919af878946a925d5a7f1046e6f030b
ramonfmir <rfernandezmir@gmail.com>
2017-11-17 00:14:09 +0000

    [CH][RF] 3440 awesome.

--- data_generator/preprocessing/permuter.py
+++ data_generator/preprocessing/permuter.py
@@ -1,6 +1,7 @@
 import glob, os
 
 import numpy as np
+import random as rand
 import cv2
 import sys
 
@@ -57,15 +58,16 @@ def permute(img_src):
 
     permed_image = deepcopy(img)
 
-    for i in range(1, len(chars_perms)):
-        for n, chars in enumerate(chars_perms[i]):
+    for i in range(50):
+        for n, chars in enumerate(chars_perms[rand.randint(0, len(chars_perms) - 1)]):
             n += 1
             space = int(max((n - 1) * char_space, 0))
             if (n < 2):
                 permed_image[ : , int(width * letter_positions[n]) : int(width * letter_positions[n]) + int(width * char_ratio)] = chars
             else:
                 permed_image[ : , int(width * letter_positions[n]) : int(width * letter_positions[n]) + int(width * char_ratio)] = chars
-        img_dst = img_src + '_' + str(i)
+        img_path, img_extension = filename, file_extension = os.path.splitext(img_src)
+        img_dst = img_path + '_' + str(i) + img_extension
         print("writing to ", img_dst)
         cv2.imwrite(img_dst, permed_image)
 
