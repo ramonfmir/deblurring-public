46cc908a928b2de4709a390d321d61b95a5fcee2
ramonfmir <rfernandezmir@gmail.com>
2017-11-18 13:17:16 +0000

    [AP][HU][RF] Super weird blurrer. But not that weird. It's aite. Many problems fixed.

--- data_generator/blurring/corrupter.py
+++ data_generator/blurring/corrupter.py
@@ -6,33 +6,28 @@ import data_generator.blurring.brightness_changer as bc
 import cv2
 import numpy as np
 import random as rand
-from copy import deepcopy
 
+width = 256
+height = 80
 
 def blur_type_1(img):
-    gaussian_kernel_size = kernel_size_corrector(rand.randint(3, 23))
-    gaussian_sd = rand.randint(1, 7)
-    motion_blur_kernel_size = kernel_size_corrector(rand.randint(5, 23))
-    motion_blur_angle = rand.uniform(0, 2 * np.pi)
-    pixelation_magnitude = rand.randint(2, 4)
+    gaussian_kernel_size = kernel_size_corrector(rand.randint(3, 17))
+    gaussian_sd = rand.randint(1, 6)
+    motion_blur_kernel_size = kernel_size_corrector(rand.randint(5, 21))
+    motion_blur_angle = rand.uniform(0, 360)
     perspective_pov = rand.uniform(-0.4, 0.4)
     resize_factor = rand.uniform(0.8, 1.00)
     contrast_level = rand.randint(1, 30)
     pixelation_magnitude = rand.randint(1, 3)
-    img = bc.change_brightness(rand.randint(0, 15), img)
 
-    # Just rotate the original.
-    original = deepcopy(img)
-    original = rs.apply_perspective(perspective_pov, original)
-    original = rs.reduce_size(resize_factor, original)
+    img, original = rs.reshape(resize_factor, perspective_pov, width, height, img)
 
     # Rotate and corrupt the corrupted.
     img = bl.gaussian_blur(gaussian_kernel_size, gaussian_sd, img)
     img = bl.motion_blur(motion_blur_kernel_size, motion_blur_angle, img)
-    img = rs.apply_perspective(perspective_pov, img)
-    img = rs.reduce_size(resize_factor, img)
     img = ct.increase_contrast(img, contrast_level)
     img = bl.pixelate_blur(pixelation_magnitude, img)
+    img = bc.change_brightness(rand.randint(0, 15), img)
 
     return original, img
 
@@ -40,19 +35,14 @@ def blur_type_1(img):
 def blur_type_2(img):
     perspective_pov = rand.uniform(-0.5, 0.5)
     resize_factor = rand.uniform(0.8, 1.00)
-    gaussian_kernel_size = kernel_size_corrector(rand.randint(6, 21))
+    gaussian_kernel_size = kernel_size_corrector(rand.randint(3, 21))
     gaussian_sd = rand.randint(3, 9)
 
-    # Just rotate the original.
-    original = deepcopy(img)
-    original = rs.apply_perspective(perspective_pov, original)
-    original = rs.reduce_size(resize_factor, original)
+    img, original = rs.reshape(resize_factor, perspective_pov, width, height, img)
 
     # Rotate and corrupt the corrupted.
-    img = rs.apply_perspective(perspective_pov, img)
-    img = rs.reduce_size(resize_factor, img)
     img = bl.gaussian_blur(gaussian_kernel_size, gaussian_sd, img)
-    img = bc.change_brightness(rand.randint(20, 100), img)
+    img = bc.change_brightness(rand.randint(50, 150), img)
 
     return original, img
 
@@ -60,18 +50,13 @@ def blur_type_2(img):
 def blur_type_3(img):
     perspective_pov = rand.uniform(-0.5, 0.5)
     resize_factor = rand.uniform(0.8, 1.00)
-    gaussian_kernel_size = kernel_size_corrector(rand.randint(11, 23))
+    gaussian_kernel_size = kernel_size_corrector(rand.randint(9, 23))
     gaussian_sd = rand.randint(4, 7)
     pixelation_magnitude = rand.randint(2, 5)
 
-    # Just rotate the original.
-    original = deepcopy(img)
-    original = rs.apply_perspective(perspective_pov, original)
-    original = rs.reduce_size(resize_factor, original)
+    img, original = rs.reshape(resize_factor, perspective_pov, width, height, img)
 
     # Rotate and corrupt the corrupted.
-    img = rs.apply_perspective(perspective_pov, img)
-    img = rs.reduce_size(resize_factor, img)
     img = bl.gaussian_blur(gaussian_kernel_size, gaussian_sd, img)
     img = bl.pixelate_blur(pixelation_magnitude, img)
 
@@ -84,25 +69,29 @@ def blur_type_4(img):
     gaussian_kernel_size = kernel_size_corrector(rand.randint(4, 9))
     gaussian_sd = rand.randint(1, 5)
     motion_blur_kernel_size = kernel_size_corrector(rand.randint(13, 23))
-    motion_blur_angle = rand.uniform(np.pi / 4, 3 * np.pi / 4) * (-1 if rand.randint(0, 1) == 0 else 1)
+    motion_blur_angle = rand.uniform(50, 130) * (-1 if rand.randint(0, 1) == 0 else 1)
 
-    # Just rotate the original.
-    original = deepcopy(img)
-    original = rs.apply_perspective(perspective_pov, original)
-    original = rs.reduce_size(resize_factor, original)
+    img, original = rs.reshape(resize_factor, perspective_pov, width, height, img)
 
     # Rotate and corrupt the corrupted.
-    img = rs.apply_perspective(perspective_pov, img)
-    img = rs.reduce_size(resize_factor, img)
     img = bl.gaussian_blur(gaussian_kernel_size, gaussian_sd, img)
     img = bl.motion_blur(motion_blur_kernel_size, motion_blur_angle, img)
 
     return original, img
 
-blurs = [blur_type_1, blur_type_2, blur_type_3, blur_type_4]
+# Nothing
+def blur_type_5(img):
+    perspective_pov = rand.uniform(-0.5, 0.5)
+    resize_factor = rand.uniform(0.8, 1.00)
+
+    img, original = rs.reshape(resize_factor, perspective_pov, width, height, img)
+
+    return original, img
+
+blurs = [blur_type_1, blur_type_2, blur_type_3, blur_type_4, blur_type_5]
 
 def corrupt(img):
-    random_blur = 0 if rand.randint(1, 100) < 85 else rand.randint(1, 3)
+    random_blur = 0 if rand.randint(1, 100) < 82 else rand.randint(1, 4)
 
     return blurs[random_blur](img)
 
@@ -114,7 +103,7 @@ def nice_goal_image(img):
     # define criteria, number of clusters(K) and apply kmeans()
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
     K = 10
-    ret,label,center = cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
     res = center[label.flatten()]
     res2 = res.reshape((img.shape))
@@ -127,11 +116,16 @@ def kernel_size_corrector(kernel_size):
     return kernel_size + 1 if kernel_size % 2 == 0 else kernel_size
 
 if __name__ == "__main__":
-    image_data = input_data.load_images("data/40nice", 270, 90)
-    input_, blurred = image_data.next_batch(1)
-    # img = cv2.imread("data/40nice/0a0a7765-f5cc-4da9-b55f-d344e3fb2671-0.jpg")
-    # img = corrupt(input_[0])
-
-    cv2.imshow('Perspective', blurred[0])
+    #image_data = input_data.load_images("data/40nice", 270, 90)
+    #input_, blurred = image_data.next_batch(1)
+    img = cv2.imread("data/40nice/0a0a7765-f5cc-4da9-b55f-d344e3fb2671-0.jpg")
+    cv2.imshow('Good', img)
     cv2.waitKey(0)
+
+    for i in range(100):
+        img = cv2.imread("data/40nice/0a0a7765-f5cc-4da9-b55f-d344e3fb2671-0.jpg")
+
+        cv2.imshow('Bad', corrupt(img)[1])
+        cv2.waitKey(0)
+
     cv2.destroyAllWindows()
--- data_generator/blurring/reshaper.py
+++ data_generator/blurring/reshaper.py
@@ -5,10 +5,12 @@ import cv2
 import scipy.misc as sc
 import random as rand
 from numpy.random import randint
+from PIL import Image
+from copy import deepcopy
 
 # Applies a 'perspective' transformation. The pov is a number between -1 and 1.
 # It will affect the angle of the perspective.
-def apply_perspective(pov, img):
+def apply_perspective(pov, val, img):
     height, width, _ = img.shape
 
     angle = pov * math.atan(height / width) / 2
@@ -28,7 +30,6 @@ def apply_perspective(pov, img):
                         [width, top_right],
                         [width, height - bottom_right]])
 
-    val = int(np.random.beta(0.5, 0.5) * 255)
     M = cv2.getPerspectiveTransform(pts_i, pts_o)
     img = cv2.warpPerspective(img, M, (width, height),
                               borderMode=cv2.BORDER_CONSTANT,
@@ -40,14 +41,13 @@ def apply_perspective(pov, img):
 # Reduce the size of the image. The magnitude is between 0 and 1. 0 is
 # equivalent to reducing the size by a half and 1 is equivalent to keeping the
 # image as it is.
-def reduce_size(magnitude, img):
+def reduce_size(magnitude, val, img):
     scale = 0.5 + (magnitude / 2)
     height, width, _ = img.shape
     new_height, new_width = int(scale * height), int(scale * width)
     v_border = (height - new_height) / 2
     h_border = (width - new_width) / 2
-
-    val = int(np.random.beta(0.5, 0.5) * 255)
+    
     img = cv2.resize(img, (new_width, new_height))
     new_img = cv2.copyMakeBorder(img, math.ceil(v_border), math.floor(v_border),
                                       math.ceil(h_border), math.floor(h_border),
@@ -56,16 +56,39 @@ def reduce_size(magnitude, img):
     return new_img
 
 
+def pad_with_val(max_size, val, old_im):
+    old_height, old_width, _ = old_im.shape
+
+    new_size_w = int((max_size[0] - old_width) / 2)
+    new_size_h = int((max_size[1] - old_height) / 2)
+
+    new_img = Image.new("RGB", max_size, "rgb(0, " + str(val) + ", 0)")
+    new_img.paste(Image.fromarray(old_im), (new_size_w, new_size_h))
+
+    return new_img
+
+def pad_with_val_and_scale(max_size, val, img):
+    img_height, img_width, _ = img.shape
+    scale = min(max_size[1] / img_height, max_size[0] / img_width)
+    new_img = cv2.resize(img, (int(scale * img_width), int(scale * img_height)))
+    new_img = pad_with_val(max_size, val, new_img)
+    return np.array(new_img)
+
 # Apply rotation.
-def rotate_image(angle, img):
-    return sc.imrotate(img, angle)
+def reshape(magnitude, pov, width, height, img):
+    val = int(np.random.beta(0.5, 0.5) * 255)
+    max_size = [width, height]
+    img = pad_with_val_and_scale(max_size, val, img)
+    img = apply_perspective(pov, val, img)
+    img = reduce_size(magnitude, val, img)
+    return img, deepcopy(img)
 
 
 if __name__ == "__main__":
-    img = cv2.imread("../tests/license_plate3.jpg")
+    img = cv2.imread("../tests/lp.jpg")
     # random_border(img)
 
-    img = reduce_size(0.95, apply_perspective(0.05, img))
+    img = reshape(0.7, 0.5, 256, 80, img)
     cv2.imshow('Perspective', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
