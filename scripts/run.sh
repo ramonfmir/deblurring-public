83b8c8e4ad6fd3f06364abcc2d7567a791dd0b88
ramonfmir <rfernandezmir@gmail.com>
2017-12-01 17:03:29 +0000

    [RF] Final CI issues fixed.

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,8 @@
 
 cd deblurring
 export PYTHONPATH=.
-pip3 install --upgrade pip
-pip3 install -r scripts/requirements_azure.txt
-python3 cnn_denoiser/train.py --run restart --num_iter 1 --model_name tutorial_cnn
+pip install --upgrade pip
+pip install -r scripts/requirements_azure.txt
+python3 cnn_denoiser/train.py --run restart --num_iter 1 --model_name moussaka
 ./scripts/slack_file_upload output.txt "#performance-metrics"
 rm output.txt
